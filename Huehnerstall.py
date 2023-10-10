ec5bd2b6fc9792bce422b5b1ae7b5f8d5fe31cb8
JRuwwe <jonas500@gmx.de>
2023-10-10 09:48:33 +0200

    Raspberry Einrichtung added

--- Huehnerstall.py
+++ Huehnerstall.py
@@ -1,3 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+import board
+import adafruit_tsl2591
+import busio
+
+# S1 ist die Steckdose am naechsten zu den Verteilerdosen
+S1 = 17  # IN1 am Relais
+S2 = 27  # IN2 am Relais
+S3 = 22  # IN3 am Relais
+S4 = 23  # IN4 am Relais
+
+# Setzt die Art der Benennung der GPIO Pins
+GPIO.setmode(GPIO.BCM)
+
+# Stellt die GPIO Pins auf Output
+GPIO.setup(S1, GPIO.OUT)
+GPIO.setup(S2, GPIO.OUT)
+GPIO.setup(S3, GPIO.OUT)
+GPIO.setup(S4, GPIO.OUT)
+
+# Setzt die GPIO Pins auf die Ausgangsstellung
+GPIO.output(S1, 0)  # An
+GPIO.output(S2, 1)  # Aus
+GPIO.output(S3, 0)  # Aus
+GPIO.output(S4, 1)  # An
+
+
 # Erstellen Sie ein I2C-Bus-Objekt.
 i2c = busio.I2C(board.SCL, board.SDA)
 
